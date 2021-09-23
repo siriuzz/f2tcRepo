9e1900cd99eede12d477894d796fb851fe702e76
siriuzzz <elianmatosd03@gmail.com>
2021-09-22 22:45:09 -0400

    agregado servidor con express

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const http = require('http')
+// pure nodejs server
+// const http = require('http')
 
-const hostname = '127.0.0.1'
-const port = 3000
+// const hostname = '127.0.0.1'
+// const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.end('Hello World\n')
+// })
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`)
+// })
+
+const express = require('express');
+const server = express();
+
+server.get('/',(req, res) => {
+  res.send('<h1>hola</h1>');
+  res.end();
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+server.get('/contact',(req, res) => {
+  res.send('<h1>hola, bienvenido a la seccion de contactos</h1>');
+  res.end();
 })
 
-console.log("hola me llamo sarcort gai")
+server.listen(3000, () => console.log('Server on port 3000'))
